4d1aeda211f218fa7ff4eaf86131ecf0a7a1cdc6
Raphael Keusch <raphael.keusch@bluewin.ch>
2025-02-14 14:47:19 +0100

    Add glob param to filter scan folders

--- pipeline.py
+++ pipeline.py
@@ -1,68 +1,60 @@
-# Run this before you launch the script: export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-
 import argparse
+import logging
 from pathlib import Path
-import ollama
 import sqlite3
-import logging
-import logging
+import glob
+from typing import Optional
+
+import ollama
 import chromadb
 import uvicorn
-
 from sentence_transformers import SentenceTransformer
-from chromadb.utils import embedding_functions
-from pathlib import Path
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
-from pathlib import Path
 
 
-ALLOWED_EXTENSIONS = [".jpg", ".jpeg"]
-
-
-def setup_logger(name, log_file, level=logging.DEBUG):
+def setup_logger(name, log_file, level=logging.INFO):
     formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-
-    # File handler
     file_handler = logging.FileHandler(log_file)
     file_handler.setFormatter(formatter)
 
-    # Console handler
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
     logger.setLevel(level)
     logger.addHandler(file_handler)
-    logger.addHandler(console_handler)  # Add console output
-
+    logger.addHandler(console_handler)
     return logger
 
 
-class Descriptor:
+class SemanticPipeline:
+    ALLOWED_EXTENSIONS = [".jpg", ".jpeg"]
+
     def __init__(self, args: argparse.Namespace):
         self.args = args
-        self.root = args.p.resolve()
-        self.output = args.o or self.root
+        self.root = args.p.resolve()  # All paths in db are relative to this
+        self.output = args.o or self.root  # Where to save semantic information
+        self.glob_pattern = args.g
         self.db_desc = self.output / "descriptions.sql"
         self.chroma_client = chromadb.PersistentClient(path=str(self.output))
         self.collection = self.chroma_client.get_or_create_collection(name="image_descriptions")
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
-        self.logger = setup_logger("Descriptor", "descriptor.log", level=logging.DEBUG)
-        self.logger.debug(f"Root: {self.root}")
+        self.model_embeddings = SentenceTransformer("all-MiniLM-L6-v2")
+        self.model_img_text = "llama3.2-vision:11b"  # ollama model name
+        self.logger = setup_logger("", "log.log", level=logging.INFO)
         self.output.mkdir(exist_ok=True)
         self.init_db()
 
-    def process_images(self):
-        self.iterate_folder(self.root)
+    def show_stats(self):
+        self.logger.info("Number of embeddings in db: " + str(self.collection.count()))
 
     def query_interactive(self, top_k=5):
-        self.logger.debug("Number of images in db: " + str(self.collection.count()))
+        self.show_stats()
 
         while True:
             text = input("Query: ").strip()
-            query_embedding = self.model.encode(text).tolist()
-            results = self.collection.query(query_embeddings=[query_embedding], n_results=3)
+            query_embedding = self.model_embeddings.encode(text).tolist()
+            results = self.collection.query(query_embeddings=[query_embedding], n_results=top_k)
             metadatas = results.get("metadatas", [[]])[0]
             similarities = results.get("distances", [[]])[0]
 
@@ -71,8 +63,8 @@ class Descriptor:
                     image_path = str(self.root / meta["path"])
                     print(f"{i}. similarity: {similarity:.4f}, {image_path}")
 
-    def query(self, query, top_k=5):
-        query_embedding = self.model.encode(query).tolist()
+    def query(self, query: str, top_k=5):
+        query_embedding = self.model_embeddings.encode(query).tolist()
         results = self.collection.query(query_embeddings=[query_embedding], n_results=top_k)
         metadatas = results.get("metadatas", [[]])[0]
         similarities = results.get("distances", [[]])[0]
@@ -91,6 +83,16 @@ class Descriptor:
         }
         return response
 
+    def get_similar_images(self, path: Path, top_k=5):
+        desc = self.get_description(path)
+        self.logger.info(f"Querying for path: {path}")
+        self.logger.info(f"Description: {desc}")
+
+        if not desc:
+            return {"results": []}
+
+        return self.query(desc, top_k)
+
     def create_embeddings(self, overwrite=False):
         collection = self.chroma_client.get_collection(name="image_descriptions")
         entries = self.get_all_entries()
@@ -102,15 +104,14 @@ class Descriptor:
             else set()
         )
 
+        n_added = 0
         for entry in entries:
             if not overwrite and entry["path"] in existing_ids:
-                self.logger.debug(f"Skipping {entry['path']}, embedding already exists.")
                 continue
 
-            self.logger.debug(f"Computing embedding for {entry['path']}")
-            embedding = self.model.encode(entry["description"]).tolist()
+            self.logger.info(f"Computing embedding for {entry['path']}")
+            embedding = self.model_embeddings.encode(entry["description"]).tolist()
 
-            # If overwriting, remove existing before adding new embedding
             if overwrite and entry["path"] in existing_ids:
                 collection.delete(where={"path": entry["path"]})
 
@@ -119,7 +120,10 @@ class Descriptor:
                 embeddings=[embedding],
                 metadatas=[{"path": entry["path"], "description": entry["description"]}],
             )
-        self.logger.debug("Embeddings created")
+            n_added += 1
+
+        self.logger.info(f"Embeddings created: {n_added}")
+        self.show_stats()
 
     def init_db(self):
         conn = sqlite3.connect(self.db_desc)
@@ -141,7 +145,7 @@ class Descriptor:
         conn.close()
         return [dict(row) for row in rows]
 
-    def path_exists(self, path: Path):
+    def path_exists_in_db(self, path: Path):
         conn = sqlite3.connect(self.db_desc)
         cursor = conn.cursor()
         path_rel = str(self.relative_path(path))
@@ -150,8 +154,18 @@ class Descriptor:
         conn.close()
         return exists
 
-    def add_path(self, path: Path, description: str):
-        if self.path_exists(path):
+    def get_description(self, path: Path) -> str | None:
+        conn = sqlite3.connect(self.db_desc)
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        path_rel = str(self.relative_path(path))
+        cursor.execute("SELECT description FROM files WHERE path = ?", (path_rel,))
+        row = cursor.fetchone()
+        conn.close()
+        return row["description"] if row else None
+
+    def save_description(self, path: Path, description: str):
+        if self.path_exists_in_db(path):
             return False
         conn = sqlite3.connect(self.db_desc)
         cursor = conn.cursor()
@@ -166,25 +180,32 @@ class Descriptor:
     def relative_path(self, path: Path):
         return path.relative_to(self.root)
 
-    def iterate_folder(self, path: Path):
-        for item in sorted(path.iterdir(), key=lambda x: x.name.lower()):
-            if item.is_dir():
-                self.logger.debug(f"Processing folder {self.relative_path(item)}")
-                self.iterate_folder(item)
-            else:
-                if item.suffix.lower() in ALLOWED_EXTENSIONS:
-                    self.logger.debug(f"Processing image {self.relative_path(item)}")
-                    if not self.path_exists(item):
-                        self.process_image(item)
+    def process_images(self):
+        pattern = str(self.root / self.glob_pattern) if self.glob_pattern else None
+        self.iterate_folder(self.root, pattern)
+
+    def iterate_folder(self, path: Path, pattern: Optional[str] = None):
+        if not pattern:
+            pattern = str(self.root / "**/*")
+
+        # We use iglob here to not load all file paths into memory
+        items = glob.iglob(pattern, recursive=True)
+        for path_str in items:
+            item = Path(path_str)
+            if item.is_file() and item.suffix.lower() in self.ALLOWED_EXTENSIONS:
+                if not self.path_exists_in_db(item):
+                    self.logger.info(f"Processing {self.relative_path(item)}")
+                    self.process_image(item)
 
     def process_image(self, image: Path):
         try:
             response = ollama.chat(
-                model="llama3.2-vision:11b",
+                model=self.model_img_text,
                 messages=[
                     {
                         "role": "user",
-                        "content": "Describe this image in great detail. Don't do lists, just create a neutral but precise description.",
+                        "content": "Describe this image in great detail. "
+                        "Don't do lists, just create a neutral but precise description.",
                         "images": [image],
                     }
                 ],
@@ -195,8 +216,8 @@ class Descriptor:
             )
             return
         desc = response["message"]["content"]
-        self.add_path(image, desc)
-        self.logger.debug(f"Description: {desc}")
+        self.save_description(image, desc)
+        # self.logger.info(f"Description: {desc}")
 
     def create_server(self):
         app = FastAPI()
@@ -216,11 +237,31 @@ class Descriptor:
         ):
             return self.query(query, top_k)
 
+        @app.get("/similar")
+        def similar_images(
+            path: str = Query(..., description="Path to image"),
+            top_k: int = Query(15, description="Number of results"),
+        ):
+            return self.get_similar_images(Path(path), top_k)
+
         return app
 
     def launch_server(self, port: int = 8000):
+        self.show_stats()
         app = self.create_server()
         uvicorn.run(app, host="127.0.0.1", port=port)
+
+    def run(self):
+        if self.args.mode == "descriptions":
+            self.process_images()
+        elif self.args.mode == "embeddings":
+            self.create_embeddings()
+        elif self.args.mode == "query":
+            self.query_interactive()
+        elif self.args.mode == "server":
+            self.launch_server()
+        else:
+            self.logger.error("Invalid mode!")
 
 
 def parse_arguments():
@@ -228,7 +269,11 @@ def parse_arguments():
     parser.add_argument(
         "mode",
         choices=["descriptions", "embeddings", "query", "server"],
-        help="Mode of operation. descriptions: process images and create descriptions, embeddings: create embeddings, query: query the database interactively, server: launch a server",
+        help="Mode of operation:\n"
+        "descriptions: process images and create descriptions\n"
+        "embeddings: create embeddings\n"
+        "query: query the database interactively\n"
+        "server: launch backend server for querying at 127.0.0.1:8000",
     )
     parser.add_argument(
         "-p", type=Path, default=Path("."), help="Path to input directory with images"
@@ -239,22 +284,19 @@ def parse_arguments():
         default=None,
         help="Output directory to save embeddings, defaults to input directory",
     )
+    parser.add_argument(
+        "-g",
+        type=str,
+        default=None,
+        help='Glob pattern to filter folders to scan, relative to input directory. Example: "images/2024/**"',
+    )
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-    d = Descriptor(args)
-    if args.mode == "descriptions":
-        d.process_images()
-    elif args.mode == "embeddings":
-        d.create_embeddings(overwrite=False)
-    elif args.mode == "query":
-        d.query_interactive()
-    elif args.mode == "server":
-        d.launch_server()
-    else:
-        d.logger.error("Invalid mode!")
+    d = SemanticPipeline(args)
+    d.run()
 
 
 if __name__ == "__main__":
